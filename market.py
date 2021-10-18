41ee7b2df33a2d5474e2256f8fe7dca7c082cbab
lukerspace.hsieh <lukerspace@gmail.com>
2021-10-18 09:39:37 +0800

    update

--- market.py
+++ market.py
@@ -1,132 +1,132 @@
-# import ccxt
-# import config
-# import schedule
-# import pandas as pd
-# pd.set_option('display.max_rows', None)
+import ccxt
+import config
+import schedule
+import pandas as pd
+pd.set_option('display.max_rows', None)
 
-# import warnings
-# warnings.filterwarnings('ignore')
+import warnings
+warnings.filterwarnings('ignore')
 
-# import numpy as np
-# from datetime import datetime
-# import time
+import numpy as np
+from datetime import datetime
+import time
 
-# exchange = ccxt.binance({
-#     "apiKey": config.BINANCE_API_KEY,
-#     "secret": config.BINANCE_SECRET_KEY
-# })
+exchange = ccxt.binance({
+    "apiKey": config.BINANCE_API_KEY,
+    "secret": config.BINANCE_SECRET_KEY
+})
+exchange.create_market_buy_order('ETH/USDT', 0.02)
+# exchange.create_market_sell_order('ETH/USDT', 0.02)
 # exchange.create_market_buy_order('ETH/USDT', 0.02)
-# # exchange.create_market_sell_order('ETH/USDT', 0.02)
-# # exchange.create_market_buy_order('ETH/USDT', 0.02)
-# import requests
-# import os
-# from dotenv import *
+import requests
+import os
+from dotenv import *
 
-# load_dotenv()
+load_dotenv()
 
 
-# WEBHOOK_URL = os.getenv("URL")
+WEBHOOK_URL = os.getenv("URL")
 
 
-# def tr(data):
-#     data['previous_close'] = data['close'].shift(1)
-#     data['high-low'] = abs(data['high'] - data['low'])
-#     data['high-pc'] = abs(data['high'] - data['previous_close'])
-#     data['low-pc'] = abs(data['low'] - data['previous_close'])
+def tr(data):
+    data['previous_close'] = data['close'].shift(1)
+    data['high-low'] = abs(data['high'] - data['low'])
+    data['high-pc'] = abs(data['high'] - data['previous_close'])
+    data['low-pc'] = abs(data['low'] - data['previous_close'])
 
-#     tr = data[['high-low', 'high-pc', 'low-pc']].max(axis=1)
+    tr = data[['high-low', 'high-pc', 'low-pc']].max(axis=1)
 
-#     return tr
+    return tr
 
-# def atr(data, period):
-#     data['tr'] = tr(data)
-#     atr = data['tr'].rolling(period).mean()
+def atr(data, period):
+    data['tr'] = tr(data)
+    atr = data['tr'].rolling(period).mean()
 
-#     return atr
+    return atr
 
-# def supertrend(df, period=7, atr_multiplier=3):
-#     hl2 = (df['high'] + df['low']) / 2
-#     df['atr'] = atr(df, period)
-#     df['upperband'] = hl2 + (atr_multiplier * df['atr'])
-#     df['lowerband'] = hl2 - (atr_multiplier * df['atr'])
-#     df['in_uptrend'] = True
+def supertrend(df, period=7, atr_multiplier=3):
+    hl2 = (df['high'] + df['low']) / 2
+    df['atr'] = atr(df, period)
+    df['upperband'] = hl2 + (atr_multiplier * df['atr'])
+    df['lowerband'] = hl2 - (atr_multiplier * df['atr'])
+    df['in_uptrend'] = True
 
-#     for current in range(1, len(df.index)):
-#         previous = current - 1
+    for current in range(1, len(df.index)):
+        previous = current - 1
 
-#         if df['close'][current] > df['upperband'][previous]:
-#             df['in_uptrend'][current] = True
-#         elif df['close'][current] < df['lowerband'][previous]:
-#             df['in_uptrend'][current] = False
-#         else:
-#             df['in_uptrend'][current] = df['in_uptrend'][previous]
+        if df['close'][current] > df['upperband'][previous]:
+            df['in_uptrend'][current] = True
+        elif df['close'][current] < df['lowerband'][previous]:
+            df['in_uptrend'][current] = False
+        else:
+            df['in_uptrend'][current] = df['in_uptrend'][previous]
 
-#             if df['in_uptrend'][current] and df['lowerband'][current] < df['lowerband'][previous]:
-#                 df['lowerband'][current] = df['lowerband'][previous]
+            if df['in_uptrend'][current] and df['lowerband'][current] < df['lowerband'][previous]:
+                df['lowerband'][current] = df['lowerband'][previous]
 
-#             if not df['in_uptrend'][current] and df['upperband'][current] > df['upperband'][previous]:
-#                 df['upperband'][current] = df['upperband'][previous]
+            if not df['in_uptrend'][current] and df['upperband'][current] > df['upperband'][previous]:
+                df['upperband'][current] = df['upperband'][previous]
         
-#     return df
+    return df
 
 
-# in_position = False
+in_position = False
 
-# def check_buy_sell_signals(df):
-#     global in_position
+def check_buy_sell_signals(df):
+    global in_position
 
-#     print("checking for buy and sell signals")
-#     print(df.tail(3))
-#     last_row_index = len(df.index) - 1
-#     previous_row_index = last_row_index - 1
+    print("checking for buy and sell signals")
+    print(df.tail(3))
+    last_row_index = len(df.index) - 1
+    previous_row_index = last_row_index - 1
 
-#     if not df['in_uptrend'][previous_row_index] and df['in_uptrend'][last_row_index]:
-#         print("changed to uptrend, buy")
-#         if not in_position:
-#             order = exchange.create_market_buy_order('ETH/USDT', 0.1)
-#             print(order)
-#             in_position = True
-#             message = f"uptrend. buy order detail: {order}"
-#             payload = {
-#                 "username": "alertbot",
-#                 "content": message
-#             }
-#             requests.post(WEBHOOK_URL, json=payload)
-#         else:
-#             print("already in position, nothing to do")
+    if not df['in_uptrend'][previous_row_index] and df['in_uptrend'][last_row_index]:
+        print("changed to uptrend, buy")
+        if not in_position:
+            order = exchange.create_market_buy_order('ETH/USDT', 0.1)
+            print(order)
+            in_position = True
+            message = f"uptrend. buy order detail: {order}"
+            payload = {
+                "username": "alertbot",
+                "content": message
+            }
+            requests.post(WEBHOOK_URL, json=payload)
+        else:
+            print("already in position, nothing to do")
     
-#     if df['in_uptrend'][previous_row_index] and not df['in_uptrend'][last_row_index]:
-#         if in_position:
-#             print("changed to downtrend, sell")
-#             order = exchange.create_market_sell_order('ETH/USDT', 0.02)
-#             print(order)
-#             in_position = False
-#             message = f"downtrend. sell order detail: {order}"
-#             payload = {
-#                 "username": "alertbot",
-#                 "content": message
-#             }
-#             requests.post(WEBHOOK_URL, json=payload)
-#         else:
-#             print("You aren't in position, nothing to sell")
+    if df['in_uptrend'][previous_row_index] and not df['in_uptrend'][last_row_index]:
+        if in_position:
+            print("changed to downtrend, sell")
+            order = exchange.create_market_sell_order('ETH/USDT', 0.02)
+            print(order)
+            in_position = False
+            message = f"downtrend. sell order detail: {order}"
+            payload = {
+                "username": "alertbot",
+                "content": message
+            }
+            requests.post(WEBHOOK_URL, json=payload)
+        else:
+            print("You aren't in position, nothing to sell")
     
-#     print("nothing to change")
+    print("nothing to change")
 
 
-# def run_bot():
-#     print(f"Fetching new bars for {datetime.now().isoformat()}")
-#     bars = exchange.fetch_ohlcv('ETH/USDT', timeframe='1m' , limit=None)
-#     df = pd.DataFrame(bars[:-1], columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
-#     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+def run_bot():
+    print(f"Fetching new bars for {datetime.now().isoformat()}")
+    bars = exchange.fetch_ohlcv('ETH/USDT', timeframe='1m' , limit=None)
+    df = pd.DataFrame(bars[:-1], columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
 
-#     supertrend_data = supertrend(df)
+    supertrend_data = supertrend(df)
     
-#     check_buy_sell_signals(supertrend_data)
+    check_buy_sell_signals(supertrend_data)
 
 
-# schedule.every(1).seconds.do(run_bot)
+schedule.every(1).seconds.do(run_bot)
 
 
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
